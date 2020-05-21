98c727d0c22556d95e81e2c1cde6d4f1eeb512ea
Sergio Romeu Agraso <a18sergiora@iessanclemente.net>
2020-05-21 21:43:18 +0200

    Added more attributes to the player

--- src/xogo/Farm.java
+++ src/xogo/Farm.java
@@ -5,14 +5,10 @@ public class Farm {
 	private int dimensionsx = 5;
 	private int dimensionsy = 5;
 	private int areas = 1;
+	private int lakes = 1;
 
 	public Farm() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- src/xogo/Main.java
+++ src/xogo/Main.java
@@ -1,9 +1,61 @@
 package xogo;
+import java.util.*;
+
 
 public class Main{
+	private static boolean stop = false;
+	public static Player player;
 
 	public static void main(String[] args) {
+		int contador = 0;
+		
+		System.out.println("Oh... hello there! Let's create a character for you!");
+		System.out.println("How do you want me to call you?");
+		
+		Scanner scanner = new Scanner(System.in);
+		String nombre = scanner.nextLine();
+		
+		System.out.println("Alright... So, hello "+nombre+"!");
+		System.out.println("Let's start playing...");
+		
+		Player player = new Player(nombre);
+		Main.player = player;
+				
+		separator();
+		
+		Farm farm = new Farm();
+		
+		System.out.println("Your new farm is watining for you!\nWhat do you want to do first?");
+		
+		menu();
+		while(stop != true) {
+			int option = scanner.nextInt();
+			if(option == 1) {
+				
+			}
+			
+		}
 		
 	}
+	
+	public static void separator() {
+		System.out.println("*-------*----------**-------*----------**-------*----------*");
+	}
+	
+	private static void stats() {
+		System.out.println("STATS:\nName: "+player.getName()+"\nMoney: "+player.getMoney()+"\nEnergy: "+player.getEnergy()+"\nExp: "+player.getExp());
+		
+	}
+	
+	public static void menu() {
+		separator();
+		//STATS
+		stats();
+		separator();
+		//MENU
+		System.out.println("Game menu! Choose an option.\n1. Mine\n2. Harvest\n3. Go to the shop.\n4. Stop playing");
+	}
+
+	
 
 }
